20af69f7e440979cad574bb556c149b3737ae779
Slowyn <y231966mdk@gmail.com>
2024-07-22 23:24:40 +0200

    Refactor camera as separate instance and class of types #8

--- app/Camera.hs
+++ app/Camera.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
-module Camera (createCamera, rayColor, GetPixelCenter, GetRay) where
+module Camera (Camera (..), CameraTrait (..)) where
 
 import Codec.Picture
 import Hittable (HitRecord (..), Hittable (..))
 import HittableList (HittableList)
-import Ray (Ray, RayTrait (..), fromCVecs)
-import Vec3 (CVec3, Vec3 (..))
+import Ray (Ray, RayTrait (..))
+import Vec3 (Vec3 (..))
 
 -- Translate the [0,1] component values to the byte range [0,255]
 vecToPixel :: forall v. (Vec3 v) => v -> PixelRGB8
@@ -41,26 +42,53 @@ calculateImageHeight width aspectRatio = max 1 imageHeight
   where
     imageHeight = floor $ fromIntegral width / aspectRatio
 
-type GetPixelCenter = Int -> Int -> CVec3
+class (Vec3 (CameraVecType c)) => CameraTrait c where
+  type CameraVecType c
+  createCamera :: Int -> Float -> c
+  render :: c -> HittableList (CameraVecType c) -> Image PixelRGB8
 
-type GetRay = CVec3 -> Ray CVec3
+data Camera v = (Vec3 v) => Camera
+  { aspectRatio :: Float,
+    width :: Int,
+    height :: Int,
+    center :: v,
+    pixel00Loc :: v,
+    pixelDeltaU :: v,
+    pixelDeltaV :: v
+  }
 
-createCamera :: Int -> (Int, GetPixelCenter, GetRay)
-createCamera imageWidth =
-  let aspectRatio :: Float = 16.0 / 9.0
-      imageHeight = calculateImageHeight imageWidth aspectRatio
+instance (Vec3 v) => CameraTrait (Camera v) where
+  type CameraVecType (Camera v) = v
+  createCamera :: Int -> Float -> Camera v
+  createCamera width aspectRatio =
+    Camera
+      { aspectRatio,
+        width,
+        height,
+        center,
+        pixel00Loc,
+        pixelDeltaU,
+        pixelDeltaV
+      }
+    where
+      height = calculateImageHeight width aspectRatio
+      center = fromXYZ (0, 0, 0)
       focalLength = 1.0
       viewportHeight = 2.0
-      viewportWidth = viewportHeight * (fromIntegral imageWidth / fromIntegral imageHeight)
-      cameraCenter = fromXYZ (0, 0, 0) :: CVec3
-      viewportU = fromXYZ (viewportWidth, 0, 0) :: CVec3
-      viewportV = fromXYZ (0, -viewportHeight, 0) :: CVec3
-      pixelDeltaU = viewportU /^ fromIntegral imageWidth
-      pixelDeltaV = viewportV /^ fromIntegral imageHeight
-      viewportUpperLeft = cameraCenter <-> fromXYZ (0, 0, focalLength) <-> viewportU /^ 2 <-> viewportV /^ 2
+      viewportWidth = viewportHeight * (fromIntegral width / fromIntegral height)
+      viewportU = fromXYZ (viewportWidth, 0, 0) :: v
+      viewportV = fromXYZ (0, -viewportHeight, 0) :: v
+      pixelDeltaU = viewportU /^ fromIntegral width
+      pixelDeltaV = viewportV /^ fromIntegral height
+      viewportUpperLeft = center <-> fromXYZ (0, 0, focalLength) <-> viewportU /^ 2 <-> viewportV /^ 2
       pixel00Loc = viewportUpperLeft <+> (pixelDeltaU <+> pixelDeltaV) .^ 0.5
-      getPixelCenter :: GetPixelCenter
-      getPixelCenter x y = pixel00Loc <+> (pixelDeltaU .^ fromIntegral x) <+> (pixelDeltaV .^ fromIntegral y)
-      getRay :: GetRay
-      getRay pixelCenter = fromCVecs cameraCenter (pixelCenter <-> cameraCenter)
-   in (imageHeight, getPixelCenter, getRay)
+
+  render :: Camera v -> HittableList v -> Image PixelRGB8
+  render camera world = generateImage renderPixel width height
+    where
+      Camera {width, height, center, pixel00Loc, pixelDeltaU, pixelDeltaV} = camera
+      renderPixel x y = rayColor ray world
+        where
+          pixelCenter = pixel00Loc <+> (pixelDeltaU .^ fromIntegral x) <+> (pixelDeltaV .^ fromIntegral y)
+          direction = pixelCenter <-> center
+          ray = fromVecs center direction
--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@
 
 module Main where
 
-import Camera (GetPixelCenter, GetRay, createCamera, rayColor)
+import Camera (Camera, CameraTrait (..))
 import Codec.Picture
 import HittableList (AnyHittable (AnyHittable), HittableList (..))
 import Sphere (Sphere (..))
@@ -12,24 +12,17 @@ import Vec3 (CVec3, Vec3 (fromXYZ))
 width :: Int
 width = 512
 
-imageCreate :: Int -> Int -> HittableList CVec3 -> GetPixelCenter -> GetRay -> Image PixelRGB8
-imageCreate width height world getPixelCenter getRay = generateImage renderPixel width height
-  where
-    renderPixel x y = rayColor ray world
-      where
-        pixelCenter = getPixelCenter x y
-        ray = getRay pixelCenter
-
 main :: IO ()
 main = do
-  let (imageHeight, getPixelCenter, getRay) = createCamera width
-      sphere1 :: Sphere CVec3
+  let sphere1 :: Sphere CVec3
       sphere1 = Sphere (fromXYZ (0, 0, -1)) 0.5
       sphere2 :: Sphere CVec3
       sphere2 = Sphere (fromXYZ (0, -100.5, -1)) 100
       world :: HittableList CVec3
       world = HittableList [AnyHittable sphere1, AnyHittable sphere2]
-      image = ImageRGB8 $ imageCreate width imageHeight world getPixelCenter getRay
+      camera :: Camera CVec3
+      camera = createCamera width (16.0 / 9.0)
+      image = ImageRGB8 $ render camera world
 
   saveJpgImage 100 "test.jpg" image
   putStrLn "Finished"
