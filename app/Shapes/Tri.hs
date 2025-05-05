d8194f092d6db304e6d44a4a574e4a78444f23ed
Slowyn <y231966mdk@gmail.com>
2025-05-05 14:40:56 +0200

    Simplify Tri getUV function

--- app/Shapes/Tri.hs
+++ app/Shapes/Tri.hs
@@ -1,8 +1,6 @@
 module Shapes.Tri (Tri (..), mkTri) where
 
 import Control.Monad (guard)
-import Interval (intervalContains, mkInterval)
-import Lib (ensure)
 import Shapes.Primitive2D (Primitive2D (..), UvTrait (..), mkStdShape)
 import Vec3 (V3)
 
@@ -10,11 +8,8 @@ data Tri = Tri deriving (Show, Eq)
 
 instance UvTrait Tri where
   getUV _ alpha beta = do
-    let unitinterval = mkInterval 0 1
-    u <- ensure (intervalContains unitinterval) alpha
-    v <- ensure (intervalContains unitinterval) beta
-    guard (alpha + beta <= 1)
-    pure (u, v)
+    guard (alpha >= 0 && beta >= 0 && alpha + beta <= 1)
+    pure (alpha, beta)
 
 mkTri :: V3 -> V3 -> V3 -> Primitive2D Tri
 mkTri q u v =
